bb1ad53f40a09360d45289982ac1f2b1716a91c5
Vagif <quliyev_vaqif@icloud.com>
2019-12-11 15:06:14 +0400

    -- Spring App Basic Structure is ready

---
+++ src/main/java/instagram/controllers/helloWorldController.java
@@ -0,0 +1,24 @@
+package instagram.controllers;
+
+
+import instagram.services.helloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloWorldController {
+    private final helloWorldService helloWorldService;
+
+    public helloWorldController(@Autowired instagram.services.helloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @GetMapping("/hello")
+    public String say_hello () {
+        return helloWorldService.talk();
+    }
+
+
+
+}
---
+++ src/main/java/instagram/services/helloWorldService.java
@@ -0,0 +1,10 @@
+package instagram.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class helloWorldService {
+    public String talk () {
+        return "Hello";
+    }
+}
