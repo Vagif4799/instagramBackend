9428ea3bbc7f4aac6e21c03d7b11f88f4db6ea22
Vagif <quliyev_vaqif@icloud.com>
2019-12-17 21:04:40 +0400

    -- contextLoads Test added

---
+++ src/main/java/app/MvcConfig.java
@@ -0,0 +1,8 @@
+package app;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+
+}
--- src/main/java/app/dao/UserRepository.java
+++ src/main/java/app/dao/UserRepository.java
@@ -4,6 +4,9 @@ import app.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+   // Collection<User> findAllByName(String name);
 }
