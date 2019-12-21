6b534819a74c72de0ca2c161d48f9461fcc3927f
ulvi0 <ulviaslan1999@gmail.com>
2019-12-21 23:53:38 +0400

    --PUT fixed

--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -80,4 +80,9 @@ import java.util.Optional;
         return Optional.of(following).map(List::size).orElse(0);
     }
 
+    @JsonProperty("count_posts")
+    private int count_posts(){
+        return Optional.of(posts).map(List::size).orElse(0);
+    }
+
 }
