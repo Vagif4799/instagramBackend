c6a25492cdbb064e7c6f3425ddb00e8a80287c13
Arzu1996 <yagut.badalova@gmail.com>
2019-12-22 16:49:49 +0400

    -- OurUserDetails Class added

--- src/main/java/app/MvcConfig.java
+++ src/main/java/app/MvcConfig.java
@@ -1,8 +1,8 @@
 package app;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
@@ -12,3 +12,4 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("login");
     }
 }
+
